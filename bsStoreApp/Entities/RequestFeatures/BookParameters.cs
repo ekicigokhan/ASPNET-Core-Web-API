e92091b0c5a3d9b1bc9079e77c198cf67d25e7ac
ekicigokhan <gokotekno48@gmail.com>
2023-11-02 12:05:16 +0300

    Search extension applied.

--- bsStoreApp/Entities/RequestFeatures/BookParameters.cs
+++ bsStoreApp/Entities/RequestFeatures/BookParameters.cs
@@ -5,6 +5,6 @@
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000;
         public bool ValidPriceRange => MaxPrice > MinPrice;
-
+        public String? SearchTerm { get; set; }
     }
 }
